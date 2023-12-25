6f34f505a9f4d9c775c410f5ce7ddf140db900b6
7yon <semyon.latysh@gmail.com>
2023-12-25 19:10:31 +0300

    Change hero bg, add Navbar and Footer

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -7,11 +7,12 @@ const CustomButton = ({
   title,
   containerStyles,
   handleClick,
+  type,
 }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
+      type={type || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
