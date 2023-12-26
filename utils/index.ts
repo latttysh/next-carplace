bef7e212494ba0625b206a164d73ad3bd7a24bfe
7yon <semyon.latysh@gmail.com>
2023-12-26 11:02:03 +0300

    server side fetch cars

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,27 @@
+import { CarProps, FilterProps } from "@/types";
 
 
-export async function fetchCars() {
-    const headers = {
-        'X-RapidAPI-Key': '2848555b98msh72e862490005260p16c6bfjsn4741a446498f',
-        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-    }
+export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', { headers: headers });
 
-    const result = await response.json()
-    return result
+    const headers: HeadersInit = {
+        "X-RapidAPI-Key": "2848555b98msh72e862490005260p16c6bfjsn4741a446498f",
+        "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    };
 
+
+    const response = await fetch(
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+        {
+            headers: headers,
+        }
+    );
+
+
+    const result = await response.json();
+
+    return result;
 }
 
 
